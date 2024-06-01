bb2d89aeae65ff254cde67e4d4333584b45e25dc
rejka permana <permanarejka@gmail.com>
2024-06-01 13:46:28 +0700

    update network response

---
+++ app/src/main/java/id/ac/unpas/managemen_keuangan/networks/responses/categories/CategoryGetResponse.kt
@@ -0,0 +1,7 @@
+package id.ac.unpas.managemen_keuangan.networks.responses.categories
+
+//import id.ac.unpas.agenda.models.Category
+
+data class CategoryGetResponse(
+    val data: List<Category>
+)
---
+++ app/src/main/java/id/ac/unpas/managemen_keuangan/networks/responses/transactions/TransactionPostResponse.kt
@@ -0,0 +1,9 @@
+package id.ac.unpas.managemen_keuangan.networks.responses.transactions
+
+//import id.ac.unpas.managemen_keuangan.models.Transaction
+
+data class TransactionPostResponse(
+    val message: String,
+    val success: Boolean,
+    val data: Transaction?
+)
