01b640c68436a1cea5f1f4b18c570c6c7073b251
aahmadzf <aahmadzf0@gmail.com>
2024-06-01 13:42:31 +0700

    Membuat model category

--- app/src/androidTest/java/id/ac/unpas/managemen_keuangan/models/categories.kt
+++ app/src/androidTest/java/id/ac/unpas/managemen_keuangan/models/categories.kt
@@ -4,14 +4,10 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.junit.runner.Description
 
-@Entity(tableName = "transaction")
-data class Transaction(
+@Entity(tableName = "categories")
+data class Categories(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
-    val amount: Double,
-    val categoryId: String,
-    val userId: String,
-    val date: Long,
     val description: String,
 )
