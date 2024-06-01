39b3e3cba96dd598f0a7f2d7e867b4889a3e78b3
rejka permana <permanarejka@gmail.com>
2024-06-01 19:41:14 +0700

    update ui

---
+++ app/src/main/java/id/ac/unpas/managemen_keuangan/ui/screens/category/FormCategoryScreen.kt
@@ -0,0 +1,74 @@
+package id.ac.unpas.managemen_keuangan.ui.screens.category
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.benasher44.uuid.uuid4
+import kotlinx.coroutines.launch
+
+@Composable
+fun FormCategoryScreen(modifier: Modifier = Modifier, id : String? = null) {
+    val viewModel = hiltViewModel<TodoViewModel>()
+    val scope = rememberCoroutineScope()
+
+    val name = remember { mutableStateOf(TextFieldValue("")) }
+    val description = remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(modifier = modifier
+        .fillMaxWidth()) {
+
+        Column(modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()) {
+            OutlinedTextField(
+                label = { Text(text = "Title") },
+                modifier = Modifier.fillMaxWidth(),
+                value = name.value, onValueChange = {
+                    name.value = it
+                })
+
+            OutlinedTextField(
+                label = { Text(text = "Description") },
+                modifier = Modifier.fillMaxWidth(),
+                value = description.value, onValueChange = {
+                    description.value = it
+                })
+
+            Row {
+                Button(modifier = Modifier.weight(5f), onClick = {
+                    if (id != null) {
+                        scope.launch {
+                            viewModel.update(id, name.value.text, description.value.text)
+                        }
+                    } else {
+                        scope.launch {
+                            viewModel.insert(uuid4().toString(), name.value.text, description.value.text)
+                        }
+                    }
+                }) {
+                    Text(text = "Simpan")
+                }
+
+                Button(modifier = Modifier.weight(5f), onClick = {
+                    name.value = TextFieldValue("")
+                    description.value = TextFieldValue("")
+                }) {
+                    Text(text = "Batal")
+                }
+            }
+        }
+    }
+
+}
